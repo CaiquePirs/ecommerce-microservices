6f6782189a716eb5be35a0f17acfdd570897e138
Caique Pires <c.pires@ba.estudante.senai.br>
2025-09-19 21:35:30 +0100

    refactor(invoicing)

--- invoicing-service/src/main/java/com/caiquepirs/invoicing/model/ItemOrder.java
+++ invoicing-service/src/main/java/com/caiquepirs/invoicing/model/ItemOrder.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 @Builder
 public record ItemOrder(
         Long id,
-        String description,
+        String name,
         BigDecimal unitValue,
         Integer quantity) {
 
