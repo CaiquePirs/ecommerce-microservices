5f6243145dfcf5c103e7c36f72255e55655d6181
Caique Pires <c.pires@ba.estudante.senai.br>
2025-09-21 21:48:07 +0100

    feat(order): implements method to calculate total order

--- orders-service/src/main/java/com/caiquepirs/orders/calculator/OrderCalculator.java
+++ orders-service/src/main/java/com/caiquepirs/orders/calculator/OrderCalculator.java
@@ -1,6 +1,7 @@
 package com.caiquepirs.orders.calculator;
 
 import com.caiquepirs.orders.model.Order;
+import com.caiquepirs.orders.model.OrderItem;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -10,8 +11,15 @@ public class OrderCalculator {
 
     public BigDecimal calculateTotalOrder(Order order){
       return order.getItems().stream()
-                .map(item -> item.getUnitValue().multiply(BigDecimal.valueOf(item.getQuantity())))
-                .reduce(BigDecimal.ZERO, BigDecimal::add)
-                .abs();
+              .map(OrderItem::getTotal)
+              .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
+    public BigDecimal calculateTotalItem(BigDecimal unitValue, Integer quantity){
+        if (unitValue == null || quantity == null) {
+            return BigDecimal.ZERO;
+        }
+        return unitValue.multiply(BigDecimal.valueOf(quantity));
+    }
+
 }
